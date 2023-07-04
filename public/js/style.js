be3ba8c24a610d1e0e758c8f4b4b96628f8f50a6
serkan <serkanbay02@hotmail.com>
2023-07-05 01:31:56 +0300

    second commit

--- public/js/style.js
+++ public/js/style.js
@@ -1,3 +1,5 @@
+// we use the IntersectionObserver, so when we scroll a bit from the start of the page, we change the background of navbar
+
 const header = document.querySelector("header");
 const scrollWatcher = document.createElement("div");
 
@@ -12,6 +14,8 @@ navObserver.observe(scrollWatcher);
 
 //intersection observer la bu hakkımızda ana sayfa altındaki çizgileri falan da değiştirebiliriz.
 
+// mobile menu
+
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const navUl = document.querySelector("nav ul");
 const socials = document.querySelector(".socials");
@@ -20,4 +24,28 @@ hamburgerMenu.addEventListener("click", () => {
   navUl.classList.toggle("show");
   socials.classList.toggle("show");
   hamburgerMenu.classList.toggle("rotate");
+});
+
+// we do this to prevent navbar background cover content
+
+const navbarHeight = document.querySelector("header nav").offsetHeight;
+
+console.log(navbarHeight);
+
+document.documentElement.style.setProperty(
+  "--scroll-padding",
+  navbarHeight + "px"
+);
+
+// checking is phone input valid
+
+const phoneInput = document.querySelector(".phone-number input");
+
+phoneInput.addEventListener("keypress", (e) => {
+  if (isNaN(e.key)) {
+    e.preventDefault();
+    phoneInput.classList.toggle("wrong-text");
+  } else {
+    phoneInput.classList.remove("wrong-text");
+  }
 });
